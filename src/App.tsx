7523eb4be47a48291174b938d9aae3a5c2aecfce
Jeffrey Li <jeffrey.cui.li@gmail.com>
2024-07-27 17:26:40 -0400

    woops

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar.tsx';
 import Home from './components/Home.tsx';
